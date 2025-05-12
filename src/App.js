cba4c807a7e574c9586915cd690956ec86800ce2
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-05-12 13:02:41 +0200

    second try footer

--- src/App.js
+++ src/App.js
@@ -11,17 +11,22 @@ function App() {
       <footer className="App-footer">
         <p>
           This App was coded by{" "}
-          <a href="https://github.com/Jo-Sa80" target="_blank">
+          <a href="https://github.com/Jo-Sa80" target="_blank" rel="noreferrer">
             Johanna Sandvoß
           </a>{" "}
           and is open sourced on{" "}
-          <a href="https://github.com/Jo-Sa80/react-weather" target="_blank">
+          <a
+            href="https://github.com/Jo-Sa80/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
             GitHub
           </a>{" "}
           and hosted on{" "}
           <a
             href="https://johannasweatherappreact.netlify.app/"
             target="_blank"
+            rel="noreferrer"
           >
             Netlify
           </a>
