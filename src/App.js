b432e702f3408ce95c64cb8093a86c8abb6ef14d
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-05-12 12:49:26 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,25 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <footer className="App-footer">
+        <p>
+          This App was coded by{" "}
+          <a href="https://github.com/Jo-Sa80" target="_blank">
+            Johanna Sandvoß
+          </a>{" "}
+          and is open sourced on{" "}
+          <a href="https://github.com/Jo-Sa80/react-weather" target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://johannasweatherappreact.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
